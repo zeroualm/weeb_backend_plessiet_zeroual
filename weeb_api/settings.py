6049cd027bbe0d41d86816ad86114c985a485f1a
Mathis Z <zeroual.mathis@gmail.com>
2026-06-03 14:37:55 +0200

    feat: login via JWT

--- weeb_api/settings.py
+++ weeb_api/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'contact',
+    'article',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
