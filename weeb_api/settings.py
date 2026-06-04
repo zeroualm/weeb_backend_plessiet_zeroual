d34fa7f4648d2131da43319a75d90d216e04f56e
Mathis Z <zeroual.mathis@gmail.com>
2026-06-04 18:03:15 +0200

    fix: adresse cors frontend

--- weeb_api/settings.py
+++ weeb_api/settings.py
@@ -58,8 +58,8 @@ MIDDLEWARE = [
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 # Authentication settings
