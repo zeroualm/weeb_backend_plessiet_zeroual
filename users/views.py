02c7eec8e9e2c7cc693d9c239ea151b6f3db0a22
Mathis Z <zeroual.mathis@gmail.com>
2026-06-04 12:58:23 +0200

    feat: signup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import SignUpSerializer
 
 # Create your views here.
+
+class SignUpView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = SignUpSerializer
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "Utilisateur crée en attente de validation"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
