b61f57abd985bd83f11940333a52a9ad4b79e7e1
Mathis Z <zeroual.mathis@gmail.com>
2026-06-05 17:39:55 +0200

    feat: refresh token http only

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from .views import SignUpView, RequestPasswordResetEmailView, PasswordResetConfirmView, LogoutView
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import SignUpView, RequestPasswordResetEmailView, PasswordResetConfirmView, LogoutView,CustomTokenView, CookieRefreshView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CustomTokenView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CookieRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('password-reset/request/', RequestPasswordResetEmailView.as_view(), name='password-reset-request'),
     path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
